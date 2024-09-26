a45ccebb755421f7980bcfe2a9c8c3c61eb7a12c
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-09-26 18:16:23 -0500

    fix(clash): api key naming convention

--- src/lambdas/app_discord/index.ts
+++ src/lambdas/app_discord/index.ts
@@ -2,31 +2,22 @@ import {getSecret} from '#src/lambdas/client-aws.ts';
 import {tryJson} from '#src/lambdas/util.ts';
 import type {AppDiscordEvent} from '#src/lambdas/types-events.ts';
 import {initDiscord} from '#src/discord/api/api-discord.ts';
-import {api_coc} from '#src/lambdas/client-api-coc.ts';
 import type {APIEmbed} from 'discord-api-types/v10';
 import {show} from '../../../util.ts';
 import {EMBED_COLOR} from '#src/discord/command-util/message-embed.ts';
 import {logError} from '#src/api/log-error.ts';
-import {COC_PASSWORD, COC_USER, DISCORD_APP_ID} from '#src/constants-secrets.ts';
+import {DISCORD_APP_ID} from '#src/constants-secrets.ts';
 import {discord, initDiscordClient} from '#src/api/api-discord.ts';
 import {dCode, dLines} from '#src/discord/command-util/message.ts';
 import {getHandlerKey} from '#src/discord/command-pipeline/commands-interaction.ts';
 import {COMMAND_HANDLERS} from '#src/discord/command-handlers.ts';
+import type {Boom} from '@hapi/boom';
 
 /**
  * @init
  */
 await initDiscordClient();
 const discord_app_id = await getSecret(DISCORD_APP_ID);
-
-const email = await getSecret(COC_USER);
-const password = await getSecret(COC_PASSWORD);
-
-await api_coc.login({
-    email,
-    password,
-    keyName: process.env.LAMBDA_ENV,
-});
 
 await initDiscord();
 
@@ -52,7 +43,7 @@ export const handler = async (event: AppDiscordEvent) => {
         });
     }
     catch (e) {
-        const error = e as Error;
+        const error = e as Error | Boom;
 
         const log = await logError(error);
 
@@ -62,7 +53,9 @@ export const handler = async (event: AppDiscordEvent) => {
             embeds: [{
                 title      : 'Error',
                 description: dLines([
-                    error.message,
+                    'isBoom' in error
+                        ? error.message
+                        : 'unknown error',
                     '',
                     `send this link to <@644290645350940692> (${dCode('yourguyryry')}) for debugging:`,
                     `<https://discord.com/channels/1283847240061947964/${log.contents.channel_id}/${log.contents.id}>`,
