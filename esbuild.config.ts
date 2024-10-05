eb1293b9ebe5d2ad0460c8827547c5f965abe851
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-10-04 22:45:53 -0500

    perf(build): rspack

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,14 +1,17 @@
 import type {BuildOptions} from 'esbuild';
 import {readdir} from 'node:fs/promises';
 import {resolve} from 'node:path';
-
-const lambdas = (await readdir('src', {withFileTypes: true, recursive: true}))
-    .filter((l) => l.name === 'index.ts')
-    .map((l) => resolve(l.parentPath, l.name));
+import {pipe} from 'effect';
+import {filter, map} from 'effect/Array';
 
 export default {
 
-    entryPoints   : lambdas,
+    entryPoints: pipe(
+        await readdir('src', {withFileTypes: true, recursive: true}),
+        filter((l) => l.name === 'index.ts'),
+        map((l) => resolve(l.parentPath, l.name)),
+    ),
+
     outdir        : 'dist',
     logLevel      : 'info',
     allowOverwrite: true,
--- rspack.config.mjs
+++ rspack.config.mjs
@@ -3,26 +3,24 @@
 import {defineConfig} from '@rspack/cli';
 import {rspack} from '@rspack/core';
 import {readdir} from 'node:fs/promises';
-import {resolve, dirname} from 'node:path';
-import pkg from './package.json' assert {type: 'json'};
+import {resolve} from 'node:path';
 import {pipe} from 'effect';
-import {reduce} from 'effect/Array';
+import {reduce, map, filter} from 'effect/Array';
+import {RsdoctorRspackPlugin} from '@rsdoctor/rspack-plugin';
 
 const targets = ['node >= 20.12'];
-
-const lambdas = (await readdir('src', {withFileTypes: true, recursive: true}))
-    .filter((l) => l.name === 'index.ts')
-    .map((l) => resolve(l.parentPath, l.name));
 
 export default defineConfig({
     mode  : 'production',
     target: 'node20.12',
 
     context: import.meta.dirname,
-    entry  : pipe(lambdas, reduce({}, (ls, l) => {
-        ls[l] = l;
-        return ls;
-    })),
+    entry  : pipe(
+        await readdir('src/aws-lambdas', {withFileTypes: true, recursive: true}),
+        filter((l) => l.name === 'index.ts'),
+        map((l) => resolve(l.parentPath, l.name).replace(import.meta.dirname, '')),
+        reduce({}, (ls, l) => (ls[l.replace('/src/aws-lambdas/', '').replace('.ts', '')] = l) && ls),
+    ),
 
     externalsType: 'module',
     externals    : [
@@ -42,8 +40,9 @@ export default defineConfig({
 
     module: {
         rules: [{
-            test: /\.js$/,
-            use : [{
+            test   : /\.js$/,
+            exclude: /node_modules/,
+            use    : [{
                 loader : 'builtin:swc-loader',
                 options: {
                     jsc: {
@@ -55,8 +54,9 @@ export default defineConfig({
                 },
             }],
         }, {
-            test: /\.ts$/,
-            use : [{
+            test   : /\.ts$/,
+            exclude: /node_modules/,
+            use    : [{
                 loader : 'builtin:swc-loader',
                 options: {
                     jsc: {
@@ -75,22 +75,15 @@ export default defineConfig({
     },
 
     plugins: [
-        new rspack.ProgressPlugin({}),
         new rspack.node.NodeTargetPlugin(),
     ],
 
     output: {
-        path       : resolve(import.meta.dirname, 'dist'),
-        clean      : true,
         module     : true,
         environment: {
             module: true,
         },
-        chunkFormat  : 'module',
-        libraryTarget: 'module',
-        library      : {
-            type: 'module',
-        },
+        clean: true,
     },
 
     experiments: {
@@ -98,40 +91,17 @@ export default defineConfig({
         topLevelAwait: true,
     },
 
-    stats: 'summary',
+    performance: {
+        hints: 'warning',
+    },
+
+    stats: {
+        preset     : 'summary',
+        entrypoints: true,
+        performance: true,
+        children   : true,
+        timings    : true,
+    },
+
+    devtool: 'nosources-cheap-module-source-map',
 });
-
-// export default defineConfig({
-//     mode: "production",
-//     entry: {
-//         index: "./src/index.js",
-//     },
-//     externalsType: "module-import",
-//     output: {
-//         module: true,
-//         chunkFormat: "module",
-//         library: {
-//             type: "modern-module",
-//         },
-//     },
-//     module: {
-//         parser: {
-//             javascript: {
-//                 importMeta: false, // keep import.meta for runtime
-//             },
-//         },
-//     },
-//     optimization: {
-//         minimize: false, // no need to minify for library
-//     },
-//     experiments: {
-//         outputModule: true
-//     },
-
-// {
-//
-//     test   : /\.m?js/,
-//     resolve: {
-//         fullySpecified: false,
-//     },
-// },
