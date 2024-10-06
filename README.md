7ac30cfce3acc764c6a02f73d41b123913df9ab4
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-10-06 03:42:37 -0500

    build: terraform validate

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # clash-discord-bot
+[![main](https://github.com/deep-fried-family-planning/clash-discord-bot/actions/workflows/main.yaml/badge.svg)](https://github.com/deep-fried-family-planning/clash-discord-bot/actions/workflows/main.yaml)
+
 Deep Fried Family Planning's Official Discord Bot
+
--- rspack.config.mjs
+++ rspack.config.mjs
@@ -14,18 +14,23 @@ export default defineConfig({
     mode  : 'production',
     target: 'node20.12',
 
-    context: import.meta.dirname,
-    entry  : pipe(
+    experiments: {outputModule: true, topLevelAwait: true},
+
+    entry: pipe(
         await readdir('src/aws-lambdas', {withFileTypes: true, recursive: true}),
         filter((l) => l.name === 'index.ts'),
         map((l) => resolve(l.parentPath, l.name).replace(import.meta.dirname, '')),
         reduce({}, (ls, l) => (ls[l.replace('/src/aws-lambdas/', '').replace('.ts', '')] = l) && ls),
     ),
 
+    output: {
+        module     : true,
+        environment: {module: true},
+        library    : {type: 'module'},
+    },
+
     externalsType: 'module',
-    externals    : [
-        /@aws-sdk./,
-    ],
+    externals    : [/@aws-sdk./],
 
     resolve: {
         tsConfig: {
@@ -38,7 +43,7 @@ export default defineConfig({
     module: {
         rules: [{
             test   : /\.js$/,
-            exclude: /node_modules\/\.pnpm/,
+            exclude: /node_modules/,
             use    : [{
                 loader : 'builtin:swc-loader',
                 options: {
@@ -52,7 +57,7 @@ export default defineConfig({
             }],
         }, {
             test   : /\.ts$/,
-            exclude: /node_modules\/\.pnpm/,
+            exclude: /node_modules/,
             use    : [{
                 loader : 'builtin:swc-loader',
                 options: {
@@ -67,41 +72,19 @@ export default defineConfig({
         }],
     },
 
-    optimization: {
-        minimizer: [new rspack.SwcJsMinimizerRspackPlugin()],
-    },
+    optimization: {minimizer: [new rspack.SwcJsMinimizerRspackPlugin()]},
 
-    plugins: [
-        new rspack.node.NodeTargetPlugin(),
-    ],
+    plugins: [new rspack.node.NodeTargetPlugin()],
 
-    output: {
-        module     : true,
-        environment: {
-            module: true,
-        },
-        library: {
-            type: 'module',
-        },
-        clean: true,
-    },
+    devtool: 'nosources-cheap-module-source-map',
 
-    experiments: {
-        outputModule : true,
-        topLevelAwait: true,
-    },
-
-    performance: {
-        hints: 'warning',
-    },
+    performance: {hints: 'warning'},
 
     stats: {
-        preset     : 'normal',
+        preset     : 'errors-only',
         entrypoints: true,
         performance: true,
         children   : true,
         timings    : true,
     },
-
-    devtool: 'nosources-cheap-module-source-map',
 });
