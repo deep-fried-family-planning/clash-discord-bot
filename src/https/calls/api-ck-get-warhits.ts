e25036bcb7aa0b92b604dec6f386b99a3931373c
martinaa23 <martinaa@sunypoly.edu>
2024-11-07 16:38:25 -0500

    Updated: schema updated to use effect

--- src/https/calls/api-ck-get-warhits.ts
+++ src/https/calls/api-ck-get-warhits.ts
@@ -1,62 +1,113 @@
 import {callClashKing} from '#src/https/api-ck.ts';
 import type {int, isodate, num, url} from '#src/pure/types-pure.ts';
 import type {CID, PID} from '#src/data/types.ts';
+import {Schema as S} from 'effect'
 
-export type CK_Player_PreviousHitsClan = {
-    tag      : CID;
-    name     : string;
-    badgeUrls: {
-        small : url;
-        large : url;
-        medium: url;
-    };
-    clanLevel            : int;
-    attacks              : int;
-    stars                : int;
-    destructionPercentage: num;
-};
 
-export type CK_Player_PreviousHitsAttack = {
-    attackerTag          : PID;
-    defenderTag          : PID;
-    stars                : int;
-    destructionPercentage: num;
-    order                : int;
-    duration             : int;
-    fresh                : boolean;
-    defender: {
-        tag            : PID;
-        name           : string;
-        townhallLevel  : int;
-        mapPosition    : int;
-        opponentAttacks: int;
-    };
-    attack_order: int;
-};
+export type CK_Player_PriorClanHits = S.Schema.Type<typeof CK_Player_PreviousHitsClan>;
 
-export type CK_Player_PreviousHits = {
-    war_data: {
-        state               : string;
-        teamSize            : int;
-        attacksPerMember    : int;
-        battleModifier      : string;
-        preparationStartTime: isodate;
-        startTime           : isodate;
-        endTime             : isodate;
-        clan                : CK_Player_PreviousHitsClan;
-        opponent            : CK_Player_PreviousHitsClan;
-        type                : 'random';
-    };
-    member_data: {
-        tag            : PID;
-        name           : string;
-        townhallLevel  : int;
-        mapPosition    : int;
-        opponentAttacks: int;
-    };
-    attacks : CK_Player_PreviousHitsAttack[];
-    defenses: CK_Player_PreviousHitsAttack[];
-};
+export const CK_Player_PreviousHitsClan = S.Struct({
+    tag: S.String,
+    name: S.String,
+    badgeUrls: S.Struct({small: S.String, large: S.String, medium: S.String}),
+    clanLevel: S.Int,
+    attacks: S.Int,
+    stars: S.Int,
+    destructionPercentage: S.Number,
+});
+
+
+// TODO
+// Not entirely sure what's wrong here
+export type CK_Player_PriorHitsAttack = S.Schema.Type<typeof CK_Player_PreviousHitsAttack>;
+
+export const CK_Player_PreviousHitsAttack = S.Struct{(
+    attackerTag : S.String,
+    _defenderTag : S.String,
+    stars : S.Int,
+    destructionPercentage : S.Number,
+    order : S.Int,
+    duration : S.Int,
+    fresh : S.Boolean,
+    defender : S.Struct ({tag : S.String, name : S.String, townhallLevel : S.Int, mapPosition: S.Int, opponentAttacks : S.Int}),
+    attack_order : S.Int,
+)};
+
+export type CK_Player_PriorHits = S.Schema.Type<typeof CK_Player_PreviousHits>;
+
+export const CK_Player_PreviousHits = S.Struct({
+   war_data: S.Struct({
+       state : S.String,
+       teamSize : S.Int,
+       attacksPerMember : S.Int,
+       battleModifier : S.String,
+       preparationStartTime : S.String,
+       startTIme : S.String,
+       endTIme : S.String,
+       clan : CK_Player_PreviousHitsClan,
+       opponent : CK_Player_PreviousHitsClan,
+       type : S.Literal('random'),
+   }),
+    attacks : CK_Player_PreviousHitsAttack[],
+    defenses: CK_Player_PreviousHitsAttack[],
+});
+
+// export type CK_Player_PreviousHitsClan = {
+//     tag      : CID;
+//     name     : string;
+//     badgeUrls: {
+//         small : url;
+//         large : url;
+//         medium: url;
+//     };
+//     clanLevel            : int;
+//     attacks              : int;
+//     stars                : int;
+//     destructionPercentage: num;
+// };
+
+
+// export type CK_Player_PreviousHitsAttack = {
+//     attackerTag          : PID;
+//     defenderTag          : PID;
+//     stars                : int;
+//     destructionPercentage: num;
+//     order                : int;
+//     duration             : int;
+//     fresh                : boolean;
+//     defender: {
+//         tag            : PID;
+//         name           : string;
+//         townhallLevel  : int;
+//         mapPosition    : int;
+//         opponentAttacks: int;
+//     };
+//     attack_order: int;
+// };
+
+// export type CK_Player_PreviousHits = {
+//     war_data: {
+//         state               : string;
+//         teamSize            : int;
+//         attacksPerMember    : int;
+//         battleModifier      : string;
+//         preparationStartTime: isodate;
+//         startTime           : isodate;
+//         endTime             : isodate;
+//         clan                : CK_Player_PreviousHitsClan;
+//         opponent            : CK_Player_PreviousHitsClan;
+//         type                : 'random';
+//     };
+//     member_data: {
+//         tag            : PID;
+//         name           : string;
+//         townhallLevel  : int;
+//         mapPosition    : int;
+//         opponentAttacks: int;
+//     };
+//     attacks : CK_Player_PreviousHitsAttack[];
+//     defenses: CK_Player_PreviousHitsAttack[];
+// };
 
 export const callPreviousHitsByPlayer = async (tag: string, limit: num) => await callClashKing<{items: CK_Player_PreviousHits[]}>({
     method: 'GET',
--- test/unit/lambdas/one-of-us.spec.ts
+++ test/unit/lambdas/one-of-us.spec.ts
@@ -10,13 +10,14 @@ const dynamoDB = DynamoDBDocument.from(base);
 describe('adrian\'s shit', () => {
     it('put test code here', async () => {
         await dynamoDB.get({
-            TableName: 'find this NAME from either terraform files or AWS', // this is NOT an ARN or amazon resource number, just the name
+            // TableName: find this NAME from either terraform files or AWS
+            TableName: 'qual-dffp-clash-discord-bot-operations', // this is NOT an ARN or amazon resource number, just the name
             Key      : {
-                pk: '',
-                sk: '',
+                pk: 'user-123', // 2nd set: user-123
+                sk: 'player-#GG123', // 2nd set: player-#GG123
             },
         });
-
         // if this fails with resource not found, your creds work
+
     });
 });
