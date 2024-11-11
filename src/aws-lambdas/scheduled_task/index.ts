cc04e11d6620fa7f17073cace71c2104a527c085
martinaa23 <martinaa@sunypoly.edu>
2024-11-11 15:49:22 -0500

    lambda: scheduler

--- src/aws-lambdas/scheduled_task/index.ts
+++ src/aws-lambdas/scheduled_task/index.ts
@@ -1,8 +1,12 @@
-import {CSL, E, L, Logger} from '#src/internals/re-exports/effect';
+import {CFG, CSL, E, L, Logger} from '#src/internals/re-exports/effect';
 import {makeLambda} from '@effect-aws/lambda';
 import {logDiscordError} from '#src/internals/errors/log-discord-error.ts';
-import {DiscordRESTMemoryLive} from 'dfx';
+import {DiscordConfig, DiscordRESTMemoryLive} from 'dfx';
 import type {SQSEvent} from 'aws-lambda';
+import {REDACTED_DISCORD_BOT_TOKEN} from '#src/internals/constants/secrets.ts';
+import {NodeHttpClient} from '@effect/platform-node';
+import {fromParameterStore} from '@effect-aws/ssm';
+import {DefaultDynamoDBDocumentServiceLayer} from '@effect-aws/lib-dynamodb';
 
 const h = (event: SQSEvent) => E.gen(function * () {
     yield * logDiscordError([new Error('hello world')]).pipe(
@@ -12,5 +16,9 @@ const h = (event: SQSEvent) => E.gen(function * () {
 });
 
 export const handler = makeLambda(h, DiscordRESTMemoryLive.pipe(
+    L.provide(NodeHttpClient.layerUndici),
+    L.provideMerge(DefaultDynamoDBDocumentServiceLayer),
+    L.provide(DiscordConfig.layerConfig({token: CFG.redacted(REDACTED_DISCORD_BOT_TOKEN)})),
+    L.provide(L.setConfigProvider(fromParameterStore())),
     L.provide(Logger.replace(Logger.defaultLogger, Logger.structuredLogger)),
 ));
