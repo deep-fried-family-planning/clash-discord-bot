cd92beb157039d5d7d6e73dbc7c3924cbb15cae2
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-11-11 19:24:01 -0600

    chore: init branch

--- src/aws-lambdas/discord_slash/commands/smoke.ts
+++ src/aws-lambdas/discord_slash/commands/smoke.ts
@@ -9,6 +9,7 @@ import {SQSService} from '@effect-aws/client-sqs';
 import {OPTION_CLAN} from '#src/aws-lambdas/discord_slash/options.ts';
 import {getDiscordClan} from '#src/database/discord-clan.ts';
 import {getAliasTag} from '#src/aws-lambdas/discord_menu/old/get-alias-tag.ts';
+import {Discord, UI} from 'dfx';
 
 export const SMOKE
     = {
@@ -30,57 +31,15 @@ export const smoke = (data: CmdIx, options: CmdOps<typeof SMOKE>) => E.gen(funct
 
     const clan = yield * getDiscordClan({pk: data.guild_id!, sk: clanTag});
 
-    const group = yield * SchedulerService.getScheduleGroup({Name: `s-${clan.pk}-c-${clan.sk.replace('#', '')}`}).pipe(E.catchAll(() => E.succeed({Name: undefined})));
-
-    yield * CSL.log(group);
-
-    if (!group.Name) {
-        const newgroup = yield * SchedulerService.createScheduleGroup({
-            Name: `s-${clan.pk}-c-${clan.sk.replace('#', '')}`,
-        });
-        yield * CSL.log(newgroup.ScheduleGroupArn);
-    }
-
-    // events
-    // immediate - create a war prep thread
-    // immediate - thread message for war managers
-    // 12 hrs - war manager reminder
-    // 24 hrs - change war prep -> battle day thread
-    // 24 hrs - do ur hits
-    // 30 hrs - eval war
-    // 36 hrs - do ur hits
-    // 36 hrs - eval war
-    // 42 hrs - eval war
-    // 46 hrs - srsly do ur hits
-    // 48 hrs - close battle day thread + results
-
-    yield * SQSService.sendMessage({
-        QueueUrl   : process.env.SQS_URL_SCHEDULED_TASK,
-        MessageBody: JSON.stringify({
-            name: 'hello world instant',
-        }),
-    });
-
-    const zoned = yield * DT.nowInCurrentZone;
-
-    const now = DT.addDuration('1 minutes')(zoned);
-
-    yield * SchedulerService.createSchedule({
-        GroupName            : `s-${clan.pk}-c-${clan.sk.replace('#', '')}`,
-        FlexibleTimeWindow   : {Mode: 'OFF'},
-        ActionAfterCompletion: 'DELETE',
-        Name                 : Date.now().toString(),
-        ScheduleExpression   : `at(${DT.formatIso(now).replace(/\..+Z/, '')})`,
-        Target               : {
-            Arn    : process.env.SQS_ARN_SCHEDULED_TASK,
-            RoleArn: process.env.LAMBDA_ROLE_ARN,
-            Input  : JSON.stringify({
-                name: 'hello world scheduled',
-            }),
-        },
-    });
-
     return {
-        embeds: [{description: 'ya did the thing'}],
+        embeds    : [{description: 'ya did the thing'}],
+        components: UI.grid([
+            [
+                UI.button({
+                    custom_id: `c-${clan.sk}`,
+                    label    : 'try me!',
+                }),
+            ],
+        ]),
     };
 });
