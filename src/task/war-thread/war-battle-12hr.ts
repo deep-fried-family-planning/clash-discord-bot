e403568e1c4eeb84700a17c595ae735b58a4211c
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-12-07 13:44:05 -0600

    chore: noiseless delivery task notifications

--- src/task/war-thread/war-battle-12hr.ts
+++ src/task/war-thread/war-battle-12hr.ts
@@ -1,72 +1,75 @@
 import {E, pipe} from '#src/internal/pure/effect.ts';
 import {makeTask, TEMP_ROLES} from '#src/task/war-thread/common.ts';
 import {DiscordApi} from '#src/discord/layer/discord-api.ts';
-import {dCrss, dHdr1, dHdr3, dLinesS, dmUser, dSubH, dtRel} from '#src/discord/util/markdown.ts';
+import {dBold, dCrss, dHdr1, dHdr2, dHdr3, dLinesS, dmUser, dSpoi, dSubH, dtRel} from '#src/discord/util/markdown.ts';
 import {dedupeL, filterL, mapL, reduceL, sortL} from '#src/internal/pure/pure-list.ts';
 import {fromCompare, OrdN} from '#src/internal/pure/pure.ts';
 import type {ClanWarAttack} from 'clashofclans.js';
 import type {ClanWarMember} from 'clashofclans.js';
 import type {snflk} from '#src/discord/types.ts';
+import {join} from 'effect/Array';
 
 
 export const WarBattle12hr = makeTask('WarBattle12hr', (data, war) => E.gen(function * () {
-    // const maxHits = war.battle.attacksPerMember;
-    // const hits = pipe(
-    //     war.battle.clan.attacks,
-    //     reduceL({} as Record<string, ClanWarAttack[]>, (acc, attack) => {
-    //         if (attack.attackerTag in acc) {
-    //             acc[attack.attackerTag].push(attack);
-    //         }
-    //         else {
-    //             acc[attack.attackerTag] = [attack];
-    //         }
-    //         return acc;
-    //     }),
-    // );
-    // const p = pipe(
-    //     war.battle.clan.members,
-    //     sortL(fromCompare<ClanWarMember>((a, b) => OrdN(a.mapPosition, b.mapPosition))),
-    //     mapL((m) => [m, `1. ${dmUser(data.links[m.tag])} (${m.name})`] as const),
-    // );
-    // yield * DiscordApi.createMessage(data.thread, {
-    //     content: dLinesS(
-    //         dHdr1(`${data.clanName} | Battle ends ${dtRel(war.battle.endTime.getTime())}`),
-    //         dSubH('strikethrough = did *all* hits'),
-    //         ...pipe(
-    //             p,
-    //             mapL((p) => {
-    //                 if (p[0].tag in hits && hits[p[0].tag].length === maxHits) {
-    //                     return `1. ${dCrss(p[1].replace('1. ', ''))}`;
-    //                 }
-    //                 return p[1];
-    //             }),
-    //         ),
-    //
-    //         dHdr3('Reminders'),
-    //         '1. Always use hero/power potions if not max',
-    //         '2. ALWAYS bring a poison spell!!!',
-    //     ),
-    //     // @ts-expect-error dfx types slightly wrong
-    //     allowed_mentions: {
-    //         roles: [
-    //             TEMP_ROLES.warmanager,
-    //             TEMP_ROLES.colead,
-    //             TEMP_ROLES.donator,
-    //             TEMP_ROLES.staff,
-    //         ] as snflk[],
-    //         users: pipe(
-    //             p,
-    //             filterL((p) => {
-    //                 if (p[0].tag in hits && hits[p[0].tag].length === maxHits) {
-    //                     return false;
-    //                 }
-    //                 return true;
-    //             }),
-    //             mapL((p) => {
-    //                 return data.links[p[0].tag] as snflk;
-    //             }),
-    //             dedupeL,
-    //         ),
-    //     },
-    // });
+    const maxHits = war.battle.attacksPerMember;
+    const hits = pipe(
+        war.battle.clan.attacks,
+        reduceL({} as Record<string, ClanWarAttack[]>, (acc, attack) => {
+            if (attack.attackerTag in acc) {
+                acc[attack.attackerTag].push(attack);
+            }
+            else {
+                acc[attack.attackerTag] = [attack];
+            }
+            return acc;
+        }),
+    );
+    const p = pipe(
+        war.battle.clan.members,
+        sortL(fromCompare<ClanWarMember>((a, b) => OrdN(a.mapPosition, b.mapPosition))),
+        mapL((m) => [m, `${dmUser(data.links[m.tag])} (${m.name})`] as const),
+    );
+    yield * DiscordApi.createMessage(data.thread, {
+        content: dLinesS(
+            dHdr1(data.clanName),
+            dHdr3(`Battle ends ${dtRel(war.battle.endTime.getTime())}`),
+            dSubH(`pings: ${pipe(
+                p,
+                mapL((p) => {
+                    if (p[0].tag in hits && hits[p[0].tag].length === maxHits) {
+                        return dCrss(p[1]);
+                    }
+                    return p[1];
+                }),
+                join(' '),
+                dSpoi,
+            )}`),
+            dHdr2('Reminders'),
+            '1. Always use hero/power potions if not max',
+            '2. ALWAYS bring a poison spell',
+            `3. ${dBold('[CWL] Check thread pinned messages for most updated base calls.')}`,
+        ),
+        // @ts-expect-error dfx types slightly wrong
+        allowed_mentions: {
+            roles: [
+                TEMP_ROLES.warmanager,
+                TEMP_ROLES.colead,
+                TEMP_ROLES.donator,
+                TEMP_ROLES.staff,
+            ] as snflk[],
+            users: pipe(
+                p,
+                filterL((p) => {
+                    if (p[0].tag in hits && hits[p[0].tag].length === maxHits) {
+                        return false;
+                    }
+                    return true;
+                }),
+                mapL((p) => {
+                    return data.links[p[0].tag] as snflk;
+                }),
+                dedupeL,
+            ),
+        },
+    });
 }));
--- src/task/war-thread/war-battle-24hr.ts
+++ src/task/war-thread/war-battle-24hr.ts
@@ -1,10 +1,11 @@
 import {E, pipe} from '#src/internal/pure/effect.ts';
 import {makeTask} from '#src/task/war-thread/common.ts';
 import {DiscordApi} from '#src/discord/layer/discord-api.ts';
-import {dHdr1, dHdr3, dLinesS, dmUser, dtRel} from '#src/discord/util/markdown.ts';
+import {dBold, dHdr1, dHdr2, dHdr3, dLinesS, dmUser, dSpoi, dSubH, dtRel} from '#src/discord/util/markdown.ts';
 import {mapL, sortL} from '#src/internal/pure/pure-list.ts';
 import {fromCompare, OrdN} from '#src/internal/pure/pure.ts';
 import type {ClanWarMember} from 'clashofclans.js';
+import {join} from 'effect/Array';
 
 
 export const WarBattle24Hr = makeTask('WarBattle24Hr', (data, war) => E.gen(function * () {
@@ -14,17 +15,20 @@ export const WarBattle24Hr = makeTask('WarBattle24Hr', (data, war) => E.gen(func
 
     yield * DiscordApi.createMessage(data.thread, {
         content: dLinesS(
-            dHdr1(`${data.clanName} | Battle ends ${dtRel(war.battle.endTime.getTime())}`),
-            ...pipe(
+            dHdr1(data.clanName),
+            dHdr3(`Battle ends ${dtRel(war.battle.endTime.getTime())}`),
+            dSubH(`pings: ${pipe(
                 war.battle.clan.members,
                 sortL(fromCompare<ClanWarMember>((a, b) => OrdN(a.mapPosition, b.mapPosition))),
-                mapL((m) => `1. ${dmUser(data.links[m.tag])} (${m.name})`),
-            ),
-
-            dHdr3('Reminders'),
+                mapL((m) => `${dmUser(data.links[m.tag])} (${m.name})`),
+                join(' '),
+                dSpoi,
+            )}`),
+            dHdr2('Reminders'),
             '1. Always use hero/power potions if not max',
-            '2. ALWAYS bring a poison spell!!!',
-            '3. [CWL only] WAIT to attack until base call strategy is ready for this war.',
+            '2. ALWAYS bring a poison spell',
+            `3. ${dBold('[CWL] WAIT to attack until base call strategy is ready for this war.')}`,
+            `4. ${dBold('[CWL] Check thread pinned messages for most updated base calls.')}`,
         ),
     });
 }));
