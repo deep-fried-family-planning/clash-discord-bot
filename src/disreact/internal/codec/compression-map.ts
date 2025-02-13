09b9f464822d6f8feb4b1ff56c5efef78ce52f6d
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2025-02-13 08:36:45 -0600

    chore: upgrades

---
+++ src/disreact/internal/codec/compression-map.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
+type CompressionMap = {
+  embeds: [];
+};
+
+
+export const makeCompressionMap = (obj: any, parentKey = '', result: Record<string, any> = {}): Record<string, any> => {
+  for (const key in obj) {
+    if (Object.hasOwn(obj, key)) {
+      const newKey = parentKey ? `${parentKey}.${key}` : key;
+
+      if (Array.isArray(obj[key])) {
+        obj[key].forEach((item, index) => {
+          const arrayKey = `${newKey}[${index}]`;
+          if (item !== null && typeof item === 'object') {
+            makeCompressionMap(item, arrayKey, result);
+          }
+          else {
+            result[arrayKey] = item;
+          }
+        });
+      }
+      else if (obj[key] !== null && typeof obj[key] === 'object') {
+        makeCompressionMap(obj[key], newKey, result);
+      }
+      else {
+        result[newKey] = obj[key];
+      }
+    }
+  }
+  return result;
+};
