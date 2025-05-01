1c9cb0cd869e62a3f9f1a5cb316beb997b1fd176
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2025-05-01 17:33:12 -0500

    chore: noice

--- config/aws.ts
+++ config/aws.ts
@@ -1,22 +1,21 @@
 import {Config} from 'effect';
 
 export const DynamoEnv = Config.all({
-  DFFP_DDB_TABLE    : Config.string('DDB_OPERATIONS'),
-  DFFP_DDB_CACHE_TTL: Config.succeed('_tag'),
-  DFFP_DDB_RCUS     : Config.succeed(10),
-  DFFP_DDB_WCUS     : Config.succeed(10),
+  DFFP_DDB_TABLE: Config.string('DDB_OPERATIONS').pipe(Config.withDefault('')),
+  DFFP_DDB_RCUS : Config.succeed(10),
+  DFFP_DDB_WCUS : Config.succeed(10),
 });
 
 export const LambdaProxyEnv = Config.all({
-  DFFP_DDB_OPERATIONS : Config.string('DDB_OPERATIONS'),
-  DFFP_APIGW_DEV_WS   : Config.string('APIGW_DEV_WS'),
+  DFFP_DDB_OPERATIONS : Config.string('DDB_OPERATIONS').pipe(Config.withDefault('')),
+  DFFP_APIGW_DEV_WS   : Config.string('APIGW_DEV_WS').pipe(Config.withDefault('')),
   DFFP_APIGW_DEV_WS_PK: Config.succeed('dev_ws'),
   DFFP_APIGW_DEV_WS_SK: Config.succeed('now'),
 });
 
 export const LambdaRoutesEnv = Config.all({
-  ix_commands  : Config.string('LAMBDA_ARN_IX_SLASH'),
-  ix_components: Config.string('LAMBDA_ARN_IX_MENU'),
-  poll         : Config.string('DFFP_LAMBDA_POLL'),
-  task         : Config.string('DFFP_LAMBDA_TASK'),
+  ix_commands  : Config.string('LAMBDA_ARN_IX_SLASH').pipe(Config.withDefault('')),
+  ix_components: Config.string('LAMBDA_ARN_IX_MENU').pipe(Config.withDefault('')),
+  poll         : Config.succeed(''),
+  task         : Config.succeed(''),
 });
--- config/external.ts
+++ config/external.ts
@@ -1,47 +1,46 @@
-import {Config} from 'effect';
+import {Config, Redacted} from 'effect';
 
 /**
  * @link https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime
  */
 export const AwsLambdaEnv = Config.all({
   // reserved
-  _HANDLER                       : Config.string('_HANDLER'),
-  _X_AMZN_TRACE_ID               : Config.string('_X_AMZN_TRACE_ID'),
-  AWS_DEFAULT_REGION             : Config.string('AWS_DEFAULT_REGION'),
-  AWS_REGION                     : Config.string('AWS_REGION'),
-  AWS_EXECUTION_ENV              : Config.string('AWS_EXECUTION_ENV'),
-  AWS_LAMBDA_FUNCTION_NAME       : Config.string('AWS_LAMBDA_FUNCTION_NAME'),
-  AWS_LAMBDA_FUNCTION_MEMORY_SIZE: Config.string('AWS_LAMBDA_FUNCTION_MEMORY_SIZE'),
-  AWS_LAMBDA_FUNCTION_VERSION    : Config.string('AWS_LAMBDA_FUNCTION_VERSION'),
-  AWS_LAMBDA_INITIALIZATION_TYPE : Config.string('AWS_LAMBDA_INITIALIZATION_TYPE'),
-  AWS_LAMBDA_LOG_GROUP_NAME      : Config.string('AWS_LAMBDA_LOG_GROUP_NAME'),
-  AWS_LAMBDA_LOG_STREAM_NAME     : Config.string('AWS_LAMBDA_LOG_STREAM_NAME'),
-  AWS_ACCESS_KEY                 : Config.string('AWS_ACCESS_KEY'),
-  AWS_ACCESS_KEY_ID              : Config.string('AWS_ACCESS_KEY_ID'),
-  AWS_SECRET_ACCESS_KEY          : Config.string('AWS_SECRET_ACCESS_KEY'),
-  AWS_SESSION_TOKEN              : Config.string('AWS_SESSION_TOKEN'),
-  AWS_LAMBDA_RUNTIME_API         : Config.string('AWS_LAMBDA_RUNTIME_API'),
-  LAMBDA_RUNTIME_DIR             : Config.string('LAMBDA_RUNTIME_DIR'),
+  _HANDLER                       : Config.string('_HANDLER').pipe(Config.withDefault('')),
+  _X_AMZN_TRACE_ID               : Config.string('_X_AMZN_TRACE_ID').pipe(Config.withDefault('')),
+  AWS_DEFAULT_REGION             : Config.string('AWS_DEFAULT_REGION').pipe(Config.withDefault('')),
+  AWS_REGION                     : Config.string('AWS_REGION').pipe(Config.withDefault('')),
+  AWS_EXECUTION_ENV              : Config.string('AWS_EXECUTION_ENV').pipe(Config.withDefault('')),
+  AWS_LAMBDA_FUNCTION_NAME       : Config.string('AWS_LAMBDA_FUNCTION_NAME').pipe(Config.withDefault('')),
+  AWS_LAMBDA_FUNCTION_MEMORY_SIZE: Config.string('AWS_LAMBDA_FUNCTION_MEMORY_SIZE').pipe(Config.withDefault('')),
+  AWS_LAMBDA_FUNCTION_VERSION    : Config.string('AWS_LAMBDA_FUNCTION_VERSION').pipe(Config.withDefault('')),
+  AWS_LAMBDA_INITIALIZATION_TYPE : Config.string('AWS_LAMBDA_INITIALIZATION_TYPE').pipe(Config.withDefault('')),
+  AWS_LAMBDA_LOG_GROUP_NAME      : Config.string('AWS_LAMBDA_LOG_GROUP_NAME').pipe(Config.withDefault('')),
+  AWS_LAMBDA_LOG_STREAM_NAME     : Config.string('AWS_LAMBDA_LOG_STREAM_NAME').pipe(Config.withDefault('')),
+  AWS_ACCESS_KEY                 : Config.string('AWS_ACCESS_KEY').pipe(Config.withDefault('')),
+  AWS_ACCESS_KEY_ID              : Config.string('AWS_ACCESS_KEY_ID').pipe(Config.withDefault('')),
+  AWS_SECRET_ACCESS_KEY          : Config.string('AWS_SECRET_ACCESS_KEY').pipe(Config.withDefault('')),
+  AWS_SESSION_TOKEN              : Config.string('AWS_SESSION_TOKEN').pipe(Config.withDefault('')),
+  AWS_LAMBDA_RUNTIME_API         : Config.string('AWS_LAMBDA_RUNTIME_API').pipe(Config.withDefault('')),
+  LAMBDA_RUNTIME_DIR             : Config.string('LAMBDA_RUNTIME_DIR').pipe(Config.withDefault('')),
   // unreserved
-  LANG                           : Config.string('LANG'),
-  PATH                           : Config.string('PATH'),
-  LD_LIBRARY_PATH                : Config.string('LD_LIBRARY_PATH'),
-  NODE_PATH                      : Config.string('NODE_PATH'),
-  PYTHONPATH                     : Config.string('PYTHONPATH'),
-  GEM_PATH                       : Config.string('GEM_PATH'),
-  AWS_XRAY_CONTEXT_MISSING       : Config.string('AWS_XRAY_CONTEXT_MISSING'),
-  AWS_XRAY_DAEMON_ADDRESS        : Config.string('AWS_XRAY_DAEMON_ADDRESS'),
-  AWS_LAMBDA_DOTNET_PREJIT       : Config.string('AWS_LAMBDA_DOTNET_PREJIT'),
-  TZ                             : Config.string('TZ'),
+  LANG                           : Config.string('LANG').pipe(Config.withDefault('')),
+  PATH                           : Config.string('PATH').pipe(Config.withDefault('')),
+  LD_LIBRARY_PATH                : Config.string('LD_LIBRARY_PATH').pipe(Config.withDefault('')),
+  NODE_PATH                      : Config.string('NODE_PATH').pipe(Config.withDefault('')),
+  PYTHONPATH                     : Config.string('PYTHONPATH').pipe(Config.withDefault('')),
+  GEM_PATH                       : Config.string('GEM_PATH').pipe(Config.withDefault('')),
+  AWS_XRAY_CONTEXT_MISSING       : Config.string('AWS_XRAY_CONTEXT_MISSING').pipe(Config.withDefault('')),
+  AWS_XRAY_DAEMON_ADDRESS        : Config.string('AWS_XRAY_DAEMON_ADDRESS').pipe(Config.withDefault('')),
+  AWS_LAMBDA_DOTNET_PREJIT       : Config.string('AWS_LAMBDA_DOTNET_PREJIT').pipe(Config.withDefault('')),
+  TZ                             : Config.string('TZ').pipe(Config.withDefault('')),
 });
 
 /**
  * @link https://developer.clashofclans.com/#/documentation
  */
 export const ClashEnv = Config.all({
-  DFFP_COC_URL      : Config.succeed('https://cocproxy.royaleapi.dev'),
-  DFFP_COC_KEY      : Config.redacted('DFFP_COC_KEY'),
-  DFFP_COC_CACHE_TTL: Config.duration('DFFP_COC_CACHE_TTL'),
+  DFFP_COC_URL: Config.succeed('https://cocproxy.royaleapi.dev'),
+  DFFP_COC_KEY: Config.redacted('DFFP_COC_KEY'),
 });
 
 /**
@@ -54,10 +53,10 @@ export const ClashKingEnv = Config.all({
 });
 
 export const DiscordEnv = Config.all({
-  DFFP_DISCORD_BOT_TOKEN : Config.redacted('DFFP_DISCORD_BOT_TOKEN'),
-  DFFP_DISCORD_PUBLIC_KEY: Config.string('DFFP_DISCORD_PUBLIC_KEY'),
-  DFFP_DISCORD_ERROR_URL : Config.string('DFFP_DISCORD_ERROR_URL'),
-  DFFP_DISCORD_DEBUG_URL : Config.string('DFFP_DISCORD_DEBUG_URL'),
+  DFFP_DISCORD_BOT_TOKEN : Config.redacted('DFFP_DISCORD_BOT_TOKEN').pipe(Config.withDefault(Redacted.make(''))),
+  DFFP_DISCORD_PUBLIC_KEY: Config.string('DFFP_DISCORD_PUBLIC_KEY').pipe(Config.withDefault('')),
+  DFFP_DISCORD_ERROR_URL : Config.string('DFFP_DISCORD_ERROR_URL').pipe(Config.withDefault('')),
+  DFFP_DISCORD_DEBUG_URL : Config.string('DFFP_DISCORD_DEBUG_URL').pipe(Config.withDefault('')),
 });
 
 export const DiscordRESTEnv = DiscordEnv.pipe(Config.map((env) =>
