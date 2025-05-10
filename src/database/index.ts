10641bf55f52f83b9d39256b39a91bf9eec97c80
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2025-05-10 02:27:23 -0500

    chore: noice

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,2 @@
 export * as DeepFryerDB from './DeepFryerDB';
 export type DeepFryerDB = typeof DeepFryerDB;
-
-export * as DeepFryerRegistry from './DeepFryerRegistry';
-export type DeepFryerRegistry = typeof DeepFryerRegistry;
