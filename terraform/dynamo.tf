c20127eddea4b23eb0fe8e4fe86806d36a5bffe6
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2025-05-19 12:46:42 -0500

    perf: flip gsi

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -23,8 +23,8 @@ resource "aws_dynamodb_table" "operations" {
     hash_key        = "pk1"
     range_key       = "sk1"
     projection_type = "ALL"
-    read_capacity   = 1
-    write_capacity  = 1
+    read_capacity   = 2
+    write_capacity  = 2
   }
   attribute {
     name = "pk1"
@@ -40,8 +40,8 @@ resource "aws_dynamodb_table" "operations" {
     hash_key        = "pk2"
     range_key       = "sk2"
     projection_type = "ALL"
-    read_capacity   = 1
-    write_capacity  = 1
+    read_capacity   = 2
+    write_capacity  = 2
   }
   attribute {
     name = "pk2"
@@ -49,62 +49,6 @@ resource "aws_dynamodb_table" "operations" {
   }
   attribute {
     name = "sk2"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name            = "GSI_ALL_SERVERS"
-    hash_key        = "gsi_all_server_id"
-    projection_type = "ALL"
-    read_capacity   = 1
-    write_capacity  = 1
-  }
-  global_secondary_index {
-    name            = "GSI_ALL_CLANS"
-    hash_key        = "gsi_clan_tag"
-    range_key       = "gsi_server_id"
-    projection_type = "ALL"
-    read_capacity   = 1
-    write_capacity  = 1
-  }
-  global_secondary_index {
-    name            = "GSI_ALL_PLAYERS"
-    hash_key        = "gsi_player_tag"
-    range_key       = "gsi_user_id"
-    projection_type = "ALL"
-    read_capacity   = 1
-    write_capacity  = 1
-  }
-  global_secondary_index {
-    name            = "GSI_USER_ROSTER_SIGNUPS"
-    hash_key        = "gsi_user_id"
-    range_key       = "gsi_roster_id"
-    projection_type = "ALL"
-    read_capacity   = 1
-    write_capacity  = 1
-  }
-  attribute {
-    name = "gsi_all_server_id"
-    type = "S"
-  }
-  attribute {
-    name = "gsi_server_id"
-    type = "S"
-  }
-  attribute {
-    name = "gsi_clan_tag"
-    type = "S"
-  }
-  attribute {
-    name = "gsi_user_id"
-    type = "S"
-  }
-  attribute {
-    name = "gsi_player_tag"
-    type = "S"
-  }
-  attribute {
-    name = "gsi_roster_id"
     type = "S"
   }
 }
